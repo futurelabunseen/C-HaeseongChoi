6d4bbdc5aa1b73eedff0b34c3d0186ddd9398c32
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-03-21 21:03:20 +0900

    MovemMurdockPlayer

--- PracticeProject/Source/PracticeProject/Characters/PPMurdockPlayer.cpp
+++ PracticeProject/Source/PracticeProject/Characters/PPMurdockPlayer.cpp
@@ -4,6 +4,9 @@
 #include "Characters/PPMurdockPlayer.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 APPMurdockPlayer::APPMurdockPlayer()
 {
@@ -30,25 +33,70 @@ APPMurdockPlayer::APPMurdockPlayer()
 	{
 		GetMesh()->SetAnimInstanceClass(AnimInstanceRef.Class);
 	}
+
+
+	// Set IMC And IA
+	ConstructorHelpers::FObjectFinder<UInputMappingContext> InputMappingContextRef(
+		TEXT("/Game/ProjectP/Input/IMC_Normal.IMC_Normal"));
+	if (InputMappingContextRef.Object)
+	{
+		DefaultMappingContext = InputMappingContextRef.Object;
+	}
+
+	ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(
+		TEXT("/Game/ProjectP/Input/Actions/IA_Move.IA_Move"));
+	if (MoveActionRef.Object)
+	{
+		MoveAction = MoveActionRef.Object;
+	}
+
+	ConstructorHelpers::FObjectFinder<UInputAction> LookActionRef(
+		TEXT("/Game/ProjectP/Input/Actions/IA_Look.IA_Look"));
+	if (LookActionRef.Object)
+	{
+		LookAction = LookActionRef.Object;
+	}
 }
 
 void APPMurdockPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APPMurdockPlayer::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APPMurdockPlayer::Look);
 }
 
 void APPMurdockPlayer::BeginPlay()
 {
 	Super::BeginPlay();
 
+	APlayerController* PlayerController = CastChecked<APlayerController>(GetController());
+	if(UEnhancedInputLocalPlayerSubsystem* Subsystem = 
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
 }
 
 void APPMurdockPlayer::Move(const FInputActionValue& Value)
 {
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	AddMovementInput(RightDirection, MovementVector.X);
 }
 
 void APPMurdockPlayer::Look(const FInputActionValue& Value)
 {
+	FVector2D LookVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookVector.X);
+	AddControllerPitchInput(LookVector.Y);
 }

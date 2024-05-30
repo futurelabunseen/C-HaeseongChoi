85ac607c548247dffcffe159e3a592a49d157d25
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-05-30 18:49:36 +0900

    Create SStrataReinforcements.cpp

---
+++ Source/SuperSoldier/Strata/SStrataReinforcements.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Strata/SStrataReinforcements.h"
+
+USStrataReinforcements::USStrataReinforcements()
+{
+	DelayTime = 5.0f;
+	CoolTime = 20.0f;
+
+	CommandArray = TArray<EStrataCommand>{ 
+		EStrataCommand::UP, 
+		EStrataCommand::DOWN, 
+		EStrataCommand::RIGHT, 
+		EStrataCommand::LEFT,
+		EStrataCommand::UP };
+}
+
+void USStrataReinforcements::ActivateStratagem(UWorld* const CurWorld, const FVector& TargetLocation)
+{
+	// Revive All Dead Player
+	UE_LOG(LogTemp, Log, TEXT("USStrataReinforcements - Revive All Dead Player"));
+}
